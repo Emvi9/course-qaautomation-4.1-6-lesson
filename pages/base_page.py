9eefea2705161d0a6f76df93e7ac125ce67ff1a6
user.name <ashotkiller227@gmail.com>
2023-07-04 22:22:22 +0300

    Changed "is_element_present" parameters

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -8,9 +10,9 @@ class BasePage:
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_present(self, selector, exception):
+    def is_element_present(self, find_by, selector):
         try:
-            self.browser.find_element(selector, exception)
+            self.browser.find_element(find_by, selector)
         except NoSuchElementException:
             return False
         return True
