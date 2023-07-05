97ad5d1fc860c9eb03d2db051f62a04648e506d6
user.name <ashotkiller227@gmail.com>
2023-07-05 23:12:55 +0300

    Made an import and changed test func

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser=browser, url="http://selenium1py.pythonanywhere.com/")
+    page = MainPage(browser=browser, url=link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
+
