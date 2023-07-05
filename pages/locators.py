690badb7e2487ab10cd93aa23fb12e70111c6b03
user.name <ashotkiller227@gmail.com>
2023-07-06 01:03:52 +0300

    Add some product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,12 @@ class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     LOGIN_EMAIL = (By.CSS_SELECTOR, "input[id='id_login-username']")
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "input[id='id_login-password']")
+
+
+class ProductPageLocators:
+    ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    CART_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
