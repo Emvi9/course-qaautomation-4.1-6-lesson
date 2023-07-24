6a2486b295a74f590ad47801f91f9393e3b37a6e
user.name <ashotkiller227@gmail.com>
2023-07-25 00:15:05 +0300

    Create a register new user method

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.base_page import BasePage
 from pages.locators import LoginPageLocators
 
@@ -17,4 +19,22 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert BasePage.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "[There is no register form on the page]"
+        assert BasePage.is_element_present(
+            *LoginPageLocators.REGISTRATION_FORM), "[There is no register form on the page]"
+
+    def register_new_user(self, email, password):
+        reg_form_email = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        reg_form_pass = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD)
+        reg_from_pass_repeat = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT)
+        submit_btn = self.browser.find_element(*LoginPageLocators.SUBMIT_BUTTON)
+
+        reg_form_email.click()
+        reg_form_email.send_keys(email)
+
+        reg_form_pass.click()
+        reg_form_pass.send_keys(password)
+
+        reg_from_pass_repeat.click()
+        reg_from_pass_repeat.send_keys(password)
+
+        submit_btn.click()
