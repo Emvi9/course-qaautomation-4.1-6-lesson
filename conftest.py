6e40b25c93d8d9708024521d6f3ed48017443ce3
user.name <ashotkiller227@gmail.com>
2023-07-25 00:18:09 +0300

    Minor change

--- conftest.py
+++ conftest.py
@@ -4,13 +4,15 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--incognito")
+# options.add_argument("--headless")
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', default='en', action='store', help='Choose your language: es / fr')
+    parser.addoption('--language', default='en', action='store', help='Choose your language: en / fr')
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope='function')
 def browser(request):
     browser_lang = request.config.getoption('language')
     options.add_experimental_option('prefs', {'intl.accept_languages': browser_lang})
